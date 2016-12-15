358e3d7e46e5071456535a0c8748da7be549f033
Eric ShangKuan <ericsk@gmail.com>
2016-12-16 01:25:19 +0800

    Completed Xamarin.iOS push notification

--- XFNotification.App.iOS/AppDelegate.cs
+++ XFNotification.App.iOS/AppDelegate.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
+using WindowsAzure.Messaging;
+
 namespace XFNotification.App.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
@@ -25,7 +27,84 @@ namespace XFNotification.App.iOS
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
+			if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+			{
+				var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+					   UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+					   new NSSet());
+
+				UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+				UIApplication.SharedApplication.RegisterForRemoteNotifications();
+			}
+			else
+			{
+				UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+				UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+			}
+
             return base.FinishedLaunching(app, options);
         }
+
+		public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+		{
+			Hub = new SBNotificationHub(Constants.ConnectionString, Constants.NotificationHubPath);
+
+			Hub.UnregisterAllAsync(deviceToken, (error) =>
+			{
+				if (error != null)
+				{
+					Console.WriteLine("Error calling Unregister: {0}", error.ToString());
+					return;
+				}
+
+				NSSet tags = null; // create tags if you want
+				Hub.RegisterNativeAsync(deviceToken, tags, (errorCallback) =>
+				{
+					if (errorCallback != null)
+						Console.WriteLine("RegisterNativeAsync error: " + errorCallback.ToString());
+				});
+			});
+		}
+
+		public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+		{
+			ProcessNotification(userInfo, false);
+		}
+
+		void ProcessNotification(NSDictionary options, bool fromFinishedLaunching)
+		{
+			// Check to see if the dictionary has the aps key.  This is the notification payload you would have sent
+			if (null != options && options.ContainsKey(new NSString("aps")))
+			{
+				//Get the aps dictionary
+				NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+
+				string alert = string.Empty;
+
+				//Extract the alert text
+				// NOTE: If you're using the simple alert by just specifying
+				// "  aps:{alert:"alert msg here"}  ", this will work fine.
+				// But if you're using a complex alert with Localization keys, etc.,
+				// your "alert" object from the aps dictionary will be another NSDictionary.
+				// Basically the JSON gets dumped right into a NSDictionary,
+				// so keep that in mind.
+				if (aps.ContainsKey(new NSString("alert")))
+					alert = (aps[new NSString("alert")] as NSString).ToString();
+
+				//If this came from the ReceivedRemoteNotification while the app was running,
+				// we of course need to manually process things like the sound, badge, and alert.
+				if (!fromFinishedLaunching)
+				{
+					//Manually show an alert
+					if (!string.IsNullOrEmpty(alert))
+					{
+						UIAlertView avAlert = new UIAlertView("Notification", alert, null, "OK", null);
+						avAlert.Show();
+					}
+				}
+			}
+		}
+
+		private SBNotificationHub Hub { get; set; }
     }
 }
---
+++ XFNotification.App.iOS/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace XFNotification.App.iOS
+{
+	public static class Constants
+	{
+		// Azure app-specific connection string and hub path
+		public const string ConnectionString = "<Azure connection string>";
+		public const string NotificationHubPath = "<Azure hub path>";
+	}
+}
