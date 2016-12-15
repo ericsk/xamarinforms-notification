64b5844335b211353c72e109ff72534005cd01b1
Eric ShangKuan <ericsk@gmail.com>
2016-12-16 00:00:07 +0800

    Completed Xamarin.Android push notification.

---
+++ XFNotification.App.Droid/Constants.cs
@@ -0,0 +1,9 @@
+namespace XFNotification.App.Droid
+{
+    public static class Constants
+    {
+        public const string SenderID = "<GoogleProjectNumber>"; // Google API Project Number
+        public const string ListenConnectionString = "<Listen connection string>";
+        public const string NotificationHubName = "<hub name>";
+    }
+}
--- XFNotification.App.Droid/MainActivity.cs
+++ XFNotification.App.Droid/MainActivity.cs
@@ -7,13 +7,21 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Android.Util;
+using Gcm.Client;
+
 namespace XFNotification.App.Droid
 {
     [Activity(Label = "XFNotification.App", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public static MainActivity instance;
+
         protected override void OnCreate(Bundle bundle)
         {
+            // Set the current instance of MainActivity.
+            instance = this;
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -21,6 +29,19 @@ namespace XFNotification.App.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
+            RegisterWithGCM();
+        }
+
+        private void RegisterWithGCM()
+        {
+            // Check to ensure everything's set up right
+            GcmClient.CheckDevice(this);
+            GcmClient.CheckManifest(this);
+
+            // Register for push notifications
+            Log.Info("MainActivity", "Registering...");
+            GcmClient.Register(this, Constants.SenderID);
         }
     }
 }
