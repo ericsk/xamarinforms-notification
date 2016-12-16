2c80e92cb3d8373b58392ec736f487e25e495467
Eric ShangKuan <ericsk@gmail.com>
2016-12-16 10:57:46 +0800

    Send notifications from ASP.NET Website.

--- XFNotification.Web/Controllers/HomeController.cs
+++ XFNotification.Web/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.Azure.NotificationHubs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using XFNotification.Web.Models;
 
 namespace XFNotification.Web.Controllers
 {
@@ -25,6 +29,35 @@ namespace XFNotification.Web.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public async Task<ActionResult> Send(string DeviceType, string Devices, string Message)
+        {
+            NotificationOutcome outcome = null;
+            HttpStatusCode ret = HttpStatusCode.InternalServerError;
+
+            switch (DeviceType)
+            {
+                case "android":
+                    var notif = "{\"data\":{\"message\":\"" + Message + "\"}}";
+                    outcome = await Notifications.Instance.Hub.SendGcmNativeNotificationAsync(notif);
+                    break;
+                case "ios":
+                    var alert = "{\"apns\":{\"alert\":\"" + Message + "\"}}";
+                    outcome = await Notifications.Instance.Hub.SendAppleNativeNotificationAsync(alert);
+                    break;
+            }
+
+            if (outcome != null)
+            {
+                if (!((outcome.State == Microsoft.Azure.NotificationHubs.NotificationOutcomeState.Abandoned) ||
+                    (outcome.State == Microsoft.Azure.NotificationHubs.NotificationOutcomeState.Unknown)))
+                {
+                    ret = HttpStatusCode.OK;
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ XFNotification.Web/Models/Notifications.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.NotificationHubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XFNotification.Web.Models
+{
+    public class Notifications
+    {
+        public static Notifications Instance = new Notifications();
+
+        public NotificationHubClient Hub { get; set; }
+
+        private Notifications()
+        {
+            Hub = NotificationHubClient.CreateClientFromConnectionString("<HUB_SEND_CONNECTIONSTRING>",
+                                                                         "<HUB_NAME>");
+        }
+    }
+}
