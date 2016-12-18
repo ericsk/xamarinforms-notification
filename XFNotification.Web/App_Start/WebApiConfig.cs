76e0bad395978c40eb8622559c3977bf1e5e2aca
Eric ShangKuan <ericsk@gmail.com>
2016-12-18 22:17:28 +0800

    Add Web API for device registrations.

--- XFNotification.Web/App_Start/WebApiConfig.cs
+++ XFNotification.Web/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace XFNotification.Web
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.MessageHandlers.Add(new AuthenticationTestHandler());
         }
     }
 }
